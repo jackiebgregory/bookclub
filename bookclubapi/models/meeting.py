cc957efdda2b32a013a0711ab04cd0a49b34e1a9
Jackie Gregory <jackiebgregory7@gmail.com>
2022-03-07 21:35:23 -0600

    seeded database

--- bookclubapi/models/meeting.py
+++ bookclubapi/models/meeting.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Meeting(models.Model):
 
-    reader = models.ManyToManyField("bookclubapi.Reader")
-    book = models.ManyToManyField("bookclubapi.Book")
+    readers = models.ManyToManyField("bookclubapi.Reader", related_name="attending")
+    book = models.ForeignKey("bookclubapi.Book", on_delete=models.CASCADE, related_name="meetings")
     date = models.DateField(max_length=20)
     time = models.TimeField(max_length=20)
     location = models.CharField(max_length=100)
